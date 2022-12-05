61bea0c8380fc4a88789b902e6ab2d716cadca85
Shadrach Jabonir <shadrachjabonir@gmail.com>
2022-12-05 21:58:11 +0700

    Initial Commit

---
+++ StaticTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StaticTableView
+//
+//  Created by Shadrach Jabonir on 05/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
